412165e6c71a006d7480e71bc70cae7079800791
sydney-dodson <sydneydodson@gmail.com>
2025-04-19 17:52:37 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
+// Project 3 - Zarraf Rayan and Sydney Dodson
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-	cout << "Hello AVL!\n";
-	return 0;
+    ifstream file("XMR.csv");
+    if (!file.is_open()) {
+        cerr << "Error: cannot open file" << endl;
+        return 1;
+    }
+
+    //initialize data structures here
+    vector<vector<string>> test;
+    string line;
+    //while(getline(file, line)){
+    for(int i = 0; i < 10; i++){
+        vector<string> row;
+        stringstream ss(line);
+        string data;
+        while(getline(ss, data, ',')){
+            row.push_back(data);
+        }
+    }
+
+    file.close();
+
+    for (auto row : test) {
+        for (auto data : row) {
+            std::cout << data << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
+    return 0;
 }
