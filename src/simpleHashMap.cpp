475bd81763c0d704c198d79b69571f40f6e3a9ab
zr4f <140024980+zr4f@users.noreply.github.com>
2025-04-19 21:47:48 +0000

    used c++'s unordered map and implemented  findMaxRange and findMinRange for the hashmap

---
+++ src/simpleHashMap.cpp
@@ -0,0 +1,94 @@
+#include "simpleHashMap.h"
+#include <sstream>
+#include <iomanip>
+
+
+simpleHashMap::simpleHashMap(){}
+void simpleHashMap::insert(const string& date, const string& time, const pair<double, double>& measurements) {
+    map[date].push_back({time, measurements});
+}
+
+// helper function to incriment the date so we  can go through the hashmap
+string simpleHashMap::incrementDate(const string& date) {
+    int year, month, day;
+    char dash;
+    stringstream ss(date);
+    ss >> year >> dash >> month >> dash >> day;
+
+    day++;
+
+
+    if (day > 31) {
+        day = 1;
+        month++;
+        if (month > 12) {
+            month = 1;
+            year++;
+        }
+    }
+
+    ostringstream oss;
+    oss << setw(4) << setfill('0') << year << "-"
+        << setw(2) << setfill('0') << month << "-"
+        << setw(2) << setfill('0') << day;
+    return oss.str();
+}
+
+
+// takes in a range of dates and returns the date of the lowest temp, time of the lowest temp, and the lowest temp as a vector
+vector<string> simpleHashMap::findMinRange(const string& date1, const string& date2) {
+    string minDate = date1;
+    string minTime = map.at(date1).front().first;
+    double minTemp = map.at(date1).front().second.first;
+    
+    vector<string> result;
+    string current = date1;
+  
+    while (current <= date2) {
+        if (map.find(current) != map.end()) {
+            for (auto i : map[current]) {
+                double temp = i.second.first;
+                if (temp < minTemp) {
+                    minTemp = temp;
+                    minDate = current;
+                    minTime = i.first;
+                }
+            }
+        }
+        current = incrementDate(current);
+    }
+
+    result.push_back(minDate);
+    result.push_back(minTime);
+    result.push_back(to_string(minTemp));
+    return result;
+}
+
+// takes in a range of dates and returns the date of the highest temp, time of the highest temp, and the highest temp as a vector
+vector<string> simpleHashMap::findMaxRange(const string& date1, const string& date2) {
+    string maxDate = date1;
+    string maxTime = map.at(date1).front().first;
+    double maxTemp = map.at(date1).front().second.first;
+    
+    vector<string> result;
+    string current = date1;
+  
+    while (current <= date2) {
+        if (map.find(current) != map.end()) {
+            for (auto i : map[current]) {
+                double temp = i.second.first;
+                if (temp > maxTemp) {
+                    maxTemp = temp;
+                    maxDate = current;
+                    maxTime = i.first;
+                }
+            }
+        }
+        current = incrementDate(current);
+    }
+
+    result.push_back(maxDate);
+    result.push_back(maxTime);
+    result.push_back(to_string(maxTemp));
+    return result;
+}
